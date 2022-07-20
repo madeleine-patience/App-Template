437180758c422c3cc9d1378484414310011892f5
Madeleine Belanger <madeleinepbelanger@gmail.com>
2022-07-19 21:17:16 -0700

    Initial push

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'star-wars-quotes',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log(`Connected to Database`)
+        db = client.db(dbName)
+        collection = db.collection('quotes')
+    })
+
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT||PORT, ()=> {
+    console.log( console.log(`Server is running. WOOO!}`))
+})
